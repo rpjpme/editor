72b38357dad52183475d308459c28cb6bf1a8081
rpjpme <rpjpme@gmail.com>
2019-06-04 15:04:27 +0200

    char * buffer replaced with vector

--- src/edit.cpp
+++ src/edit.cpp
@@ -7,26 +7,26 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     WithStdscr main_window = WithStdscr::CreateResource();
-    char* buffer = new char[1024];
-    char* p_buf = buffer;
-    const char* const p_end = &buffer[1023];
-    bool exit = false;
-    int key = 0;
     int height, width;
-    int cur_h, cur_w;
     getmaxyx(stdscr, height, width);
+    //    char* buffer = new char[1024];
+    vector<char> buffer(height * width, 0);
+    auto ibuf = buffer.begin(); // get iterator of class vector
+    int key = 0;
+    bool exit = false;
+    int cur_h, cur_w;
     while (!exit) {
         key = getch();
         switch (key) {
         case KEY_BACKSPACE:
-            if (p_buf != buffer) {
-                *p_buf = (char)0; // add cast so, not to confuse with setting the pointer to  0/NULL
-                --p_buf;
+            if (ibuf != ibuf_end) {
+                *ibuf = (char)0; // add cast so, not to confuse with setting the pointer to  0/NULL
+                --ibuf;
             }
             break;
         case 27:
         case '`': // backup for debug purposes.
-            exit = TRUE;
+            exit = true;
         case KEY_ENTER:
             if (++cur_h > height) {
                 cur_h = 0; // end of screen go back to start.
@@ -44,15 +44,15 @@ int main(int argc, char* argv[])
                 };
             };
 */
-                *p_buf = (char)key;
-                ++p_buf;
-                if (p_buf == p_end)
-                    p_buf = buffer; // for now we have a circular buffer.
+                *ibuf = (char)key;
+                ++ibuf;
+                if (ibuf == buffer.end())
+                    ibuf = buffer.begin(); // for now we have a circular buffer.
             };
         };
         move(0, 0);
         wclear(stdscr);
-        waddstr(stdscr, buffer);
+        waddstr(stdscr, buffer.data());
         refresh();
     }
     return 0;
