a3584c4de3c19e5e6867110ce2482e2a99d8db96
rpjpme <rpjpme@gmail.com>
2019-06-11 14:51:42 +0200

    added a Log Class hpp file.

---
+++ include/log.hpp
@@ -0,0 +1,48 @@
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+
+enum LogLevel {
+    info = 0,
+    debug,
+    warning,
+    error
+};
+
+class Log {
+    static std::ofstream out;
+    static int counter;
+    static std::time_t start_time;
+    std::string file_name;
+    LogLevel log_level;
+    Log(LogLevel lev = info, const char* fname = "tmp.log")
+        : log_level(lev)
+        , file_name(fname)
+    {
+        ++counter;
+        if (!out) {
+            out.open(file_name, std::ios::out | std::ios::trunc);
+            start_time = std::time(nullptr);
+        };
+        out << "Start Log file at:" << std::ctime(&start_time) << '\n';
+    };
+    void log(LogLevel level, const char* msg)
+    {
+        std::string level_s = (level == info ? "Info" : level == debug ? "Debug" : level == warning ? "Warning" : "Error");
+        std::time_t now = std::time(nullptr);
+        double dt_seconds = std::difftime(now, start_time);
+        out << level_s << ": @" << dt_seconds << "||" << msg << "||\n";
+    };
+
+    ~Log()
+    {
+        if (--counter == 0) {
+            out.close();
+        }
+    };
+};
+#endif
